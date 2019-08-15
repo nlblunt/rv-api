d2fb01a3e2060d8b04377e5bcaf30b2804edcd55
Nicolas Willis-Blunt <nicolas.blunt@gmail.com>
2019-08-14 18:32:13 -0700

    Swimming Pool

--- db/migrate/20190815004337_add_to_rvpark081419.rb
+++ db/migrate/20190815004337_add_to_rvpark081419.rb
@@ -3,6 +3,7 @@ class AddToRvpark081419 < ActiveRecord::Migration[5.2]
     add_column :rvparks, :cash, :boolean
     add_column :rvparks, :boating, :boolean
     add_column :rvparks, :biking, :boolean
+    add_column :rvparks, :swimmingpool, :boolean
     add_column :rvparks, :gps, :string
   end
 end
