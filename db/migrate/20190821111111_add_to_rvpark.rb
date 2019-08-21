2f92f143e1351d9218e1f7901c03a176eb71cf49
Nicolas Willis-Blunt <nicolas.blunt@gmail.com>
2019-08-21 09:07:56 -0700

    Added additional tracking fields to Rvpark

---
+++ db/migrate/20190821111111_add_to_rvpark.rb
@@ -0,0 +1,8 @@
+class AddToRvpark < ActiveRecord::Migration[5.2]
+  def change
+    add_column :rvparks, :cancellation, :boolean
+	add_column :rvparks, :wildlife, :boolean
+	add_column :rvparks, :extravehiclefee, :boolean
+	add_column :rvparks, :elevation, :string
+  end
+end
