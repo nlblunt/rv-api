5e34e47afafe86844dc67ff7cf5656ca45c87aef
Nicolas Willis-Blunt <nicolas.blunt@gmail.com>
2019-08-29 20:24:58 -0700

    Update drawing_controller.rb

--- app/controllers/drawing_controller.rb
+++ app/controllers/drawing_controller.rb
@@ -1,40 +1,49 @@
 class DrawingController < ApplicationController
   def addentry
-    user = Drawing.find_by_email(params[:email])
+    user = Drawing.where(:email => params[:email])
 
-    found = false
-    user.each do |u|
-      if u.giveawayid == params[:giveawayid]
-        u.count++
-        found = true
-        render json: {:count => u.count}
+    found_giveaway = false
+    if user.blank?
+      pp "No entries for this user"
+      Drawing.create!(:name => params[:name], :email => params[:email], :giveawayid => params[:giveawayid], :count => params[:count])
+    else
+      pp "Entreis for this user"
+      user.each do |u|
+        pp u.giveawayid
+        pp params[:giveawayid]
+        if u.giveawayid == params[:giveawayid].to_i
+          u.count = u.count + 1
+          u.save
+          pp u.count
+          found_giveaway = true
+        end
       end
-    end
-
-    if found == false
-      Drawing.create(drawing_params)
-      render json: {:count => 1}
+      if found_giveaway == false
+        Drawing.create!(:name => params[:name], :email => params[:email], :giveawayid => params[:giveawayid], :count => params[:count])
+      end
     end
   end
 
   def getentrycount
     user = Drawing.where(:email => params[:email])
     
+    count = 0
     if user.blank?
-      render json: {:count => 0}
-    end
-    
-    user.each do |u|
-      if u.giveawayid == params(:giveawayid)
-        render json: {:count => u.count}
-      else
-        render json: {:count => 0}
+      count = 0
+    else
+      user.each do |u|
+        if u.giveawayid == params[:giveawayid].to_i
+          count = u.count
+        end
       end
     end
+
+    pp count
+    render json: {:count => count}
   end
 
   private
   def drawing_params
-    params.require(:chamberdrawing).permit(:name, :email, :month, :giveawayid, :count)
+    params.require(:drawing).permit(:name, :email, :month, :giveawayid, :count)
   end
 end
